59f470f35a9ad52ac023924f7e3d4abfb01dd989
Aditya Narayan <an160264@gmail.com>
2019-07-23 04:10:22 -0700

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,45 @@
+//Author:Aditya Narayan
+//Date:23-07-2019
+let values = [];
+
+let i = 0;
+let j = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  values = new Array(width);
+  for (let i = 0; i < values.length; i++) {
+    values[i] = random(height);
+    // values[i] = noise(i/100.0)*height+123.44;
+  }
+}
+
+function draw() {
+  background(100,190,20);
+
+  if (i < values.length) {
+    for (let j = 0; j < values.length - i - 1; j++) {
+      let a = values[j];
+      let b = values[j + 1];
+      if (a > b) {
+        swap(values, j, j + 1);
+      }
+    }
+  } else {
+    console.log("finished");
+    noLoop();
+  }
+  i++;
+
+  for (let i = 0; i < values.length; i++) {
+    stroke(255,25,255);
+    line(i, height, i, height - values[i]);
+  }
+  console.log(values);
+}
+
+function swap(arr, a, b) {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+}
